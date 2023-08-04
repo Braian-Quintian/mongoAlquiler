6dc0b94efd2d09f47686a786ee61930a411e0bc7
Braian-Quintian <bquintianromero@gmail.com>
2023-08-04 13:27:39 -0500

    feat: :sparkles: Se añade el routes para alquileres por el id

--- src/routes/alquileres.routes.js
+++ src/routes/alquileres.routes.js
@@ -33,6 +33,40 @@ const getAlquileres = async (req, res) => {
     }
 }
 
+const getAlquilerById = async (req, res) => {
+    if (!req.rateLimit) return;
+    try {
+        let db = await connect();
+        let alquiler = db.collection("Alquiler");
+
+        let result = await alquiler.aggregate([
+            { $match: { id: parseInt(req.params.id) } },
+            {
+                $lookup: {
+                  from: "Cliente",
+                  localField: "ID_Cliente",
+                  foreignField: "id",
+                  as: "cliente"
+                }
+            },
+            { $unwind: "$cliente"},
+            {
+                $project: {
+                 _id: 0,
+                Fecha_Inicio: 0,
+                Fecha_Fin:0,
+                 "cliente._id": 0
+                }
+            }
+        ]).toArray();
+
+        res.json(result);
+    }catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 export const methodsAlquileres = {
-    getAlquileres
+    getAlquileres,
+    getAlquilerById
 }
