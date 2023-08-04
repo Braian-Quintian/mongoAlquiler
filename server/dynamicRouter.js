2970ea65c6caee3329ae7ad4b425ded7391b2b3f
Braian-Quintian <bquintianromero@gmail.com>
2023-08-04 11:22:30 -0500

    feat: :sparkles: Se añade el enpoint reservas

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,6 +1,7 @@
 import { router as Clientes } from '../src/middlewares/clientes.js'
 import { router as Automovil } from '../src/middlewares/automoviles.js'
 import { router as Alquileres } from '../src/middlewares/alquileres.js'
+import { router as Reservas } from '../src/middlewares/reservas.js'
 
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
@@ -12,6 +13,8 @@ export async function dynamicRouter(req, res, next) {
             return Automovil(req, res, next);
         case 'alquileres':
             return Alquileres(req, res, next);
+        case 'reservas':
+            return Reservas(req, res, next);
         default:
             return res.status(404).send({ error: 'Ruta no encontrada' });
     }
