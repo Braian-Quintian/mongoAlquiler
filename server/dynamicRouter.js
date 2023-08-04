a1f699e37a917f27394a2178439145c17592b65e
Braian-Quintian <bquintianromero@gmail.com>
2023-08-04 10:45:47 -0500

    feat: :sparkles: Se añaden los endpoints para automoviles y alquileres

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,4 +1,6 @@
 import { router as Clientes } from '../src/middlewares/clientes.js'
+import { router as Automovil } from '../src/middlewares/automoviles.js'
+import { router as Alquileres } from '../src/middlewares/alquileres.js'
 
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
@@ -6,6 +8,10 @@ export async function dynamicRouter(req, res, next) {
     switch (collection) {
         case 'clientes':
             return Clientes(req, res, next);
+        case 'automoviles':
+            return Automovil(req, res, next);
+        case 'alquileres':
+            return Alquileres(req, res, next);
         default:
             return res.status(404).send({ error: 'Ruta no encontrada' });
     }
