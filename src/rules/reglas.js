b3bdb519c308c2f659f64fa95c8bc5b52f00c505
Braian-Quintian <bquintianromero@gmail.com>
2023-08-04 10:51:15 -0500

    feat: :sparkles: Limite de peticiones

--- src/rules/reglas.js
+++ src/rules/reglas.js
@@ -2,8 +2,8 @@ import rateLimit from 'express-rate-limit';
 
 export let limitGet = () => {
     return rateLimit({
-        windowMs: 30 * 1000, // 30 seconds
-        max: 25, // 25 requests,
+        windowMs: 60 * 1000, // 60 seconds
+        max: 10, // 10 requests,
         standardHeaders: true, // Return rate limit info in the RateLimit- headers
         legacyHeaders: false, // Disable the 'X-RateLimit' legacy header
         skip: (req, res) => req.headers["content-length"] > 91
