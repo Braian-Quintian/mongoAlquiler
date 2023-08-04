6ea6e660024b0e818b4da5a6107a6119b9c4ffbc
Braian-Quintian <bquintianromero@gmail.com>
2023-08-04 13:54:38 -0500

    feat: :sparkles: Se añade el router para los autmoviles

--- src/routes/automoviles.routes.js
+++ src/routes/automoviles.routes.js
@@ -34,6 +34,43 @@ const getAutomoviles = async (req, res) => {
     }
 };
 
+const getAutomovilesDisponibles = async (req, res) => {
+    if (!req.rateLimit) return;
+    try {
+        let db = await connect();
+        let automovil = db.collection("Sucursal_Automovil");
+
+        let result = await automovil.aggregate([
+            {
+                $group: {
+                    _id: "$ID_Sucursal",
+                    Cantidad_Total_Disponible: { $sum: "$Cantidad_Disponible" }
+                }
+            },
+            {
+                $lookup: {
+                    from: "Sucursal",
+                    localField: "_id",
+                    foreignField: "id",
+                    as: "sucursal"
+                }
+            },
+            { $unwind: "$sucursal" },
+            {
+                $project: {
+                    _id: 0,
+                    Sucursal: "$sucursal.Nombre",
+                    Cantidad_Total_Disponible: 1
+                }
+            }
+        ]);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 export const methodsAutomoviles = {
     getAutomoviles,
+    getAutomovilesDisponibles
 };
