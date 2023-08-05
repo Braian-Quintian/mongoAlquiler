6ab549b15d7f89f20e4a1c98dd1e167d14a75958
Braian-Quintian <bquintianromero@gmail.com>
2023-08-05 10:42:39 -0500

    feat: :sparkles: Se añade el enpoint para traer a los empleados que tienen el cargo de asistente o gerente

--- src/middlewares/empleados.js
+++ src/middlewares/empleados.js
@@ -3,6 +3,7 @@ import { methodsEmpleados } from '../routes/empleados.routes.js';
 const router = Router();
 
 router.get('/', methodsEmpleados.getEmpleados);
+router.get('/otros', methodsEmpleados.getOtrosEmpleados);
 
 export {
     router
--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -13,6 +13,21 @@ const getEmpleados = async (req, res) => {
     }
 }
 
+const getOtrosEmpleados = async (req, res) => {
+    if(!req.rateLimit) return;
+    try{
+        let db = await connect();
+        let empleados = db.collection("Empleado");
+
+        let result = await empleados.find({$or: [ { Cargo: "Gerente" }, { Cargo: "Asistente"} ] }, { _id: 0 }).toArray();
+        res.json(result);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+
 export const methodsEmpleados ={
-    getEmpleados
+    getEmpleados,
+    getOtrosEmpleados
 }
