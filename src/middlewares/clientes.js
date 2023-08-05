02eb91cea94a88a50a6bee843d59257b90acfb0d
Braian-Quintian <bquintianromero@gmail.com>
2023-08-05 09:25:30 -0500

    feat: :sparkles: Se añade el endpoint para buscar por dni

--- src/middlewares/clientes.js
+++ src/middlewares/clientes.js
@@ -3,6 +3,7 @@ import { methodsClientes } from '../routes/clientes.routes.js';
 const router = Router();
 
 router.get('/', methodsClientes.getClientes);
+router.get('/:id', methodsClientes.getClienteById);
 
 export {
     router
