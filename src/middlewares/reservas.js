2de33727f36f5f7f22db073a4088d3090e8ec38c
Braian-Quintian <bquintianromero@gmail.com>
2023-08-05 10:28:23 -0500

    feat: :sparkles: Se crea el endpoint para traer la reserva con el cliente especifico

--- src/middlewares/reservas.js
+++ src/middlewares/reservas.js
@@ -3,6 +3,7 @@ import { methodsReservas } from '../routes/reservas.routes.js';
 const router = Router();
 
 router.get('/', methodsReservas.getReservas);
+router.get('/:id', methodsReservas.getReservasById);
 
 export {
     router
--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -44,7 +44,40 @@ const getReservas = async (req, res) => {
     }
 }
 
+const getReservasById = async (req, res) => {
+    if(!req.rateLimit) return;
+    try{
+        let db = await connect();
+        let reserva =db.collection("Reserva");
+
+        let result = await reserva.aggregate([
+            { $match: {$and: [{ID_Cliente: parseInt(req.params.id)},{ Estado: "Pendiente" }]} },
+            {
+                $lookup: {
+                    from: "Cliente",
+                    localField: "ID_Cliente",
+                    foreignField: "id",
+                    as: "cliente"
+                }
+            },
+            { $unwind: "$cliente"},
+            {
+                $project: {
+                    _id: 0,
+                    Fecha_Inicio: 0,
+                    Fecha_Fin:0,
+                    Fecha_Reserva: 0,
+                    "cliente._id": 0,
+                }
+            }
+        ]).toArray();
+        res.json(result)
+    }catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 
 export const methodsReservas = {
-    getReservas
+    getReservas,
+    getReservasById
 }
