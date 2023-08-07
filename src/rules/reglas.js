a8b8f86ad6e9e69ffafc9bcbe42a4b01729b6491
Braian-Quintian <bquintianromero@gmail.com>
2023-08-07 11:50:44 -0500

    feat: :sparkles: Se aumenta la cantidad de caracteres permitido por envio

--- src/rules/reglas.js
+++ src/rules/reglas.js
@@ -6,7 +6,7 @@ export let limitGet = () => {
         max: 10, // 10 requests,
         standardHeaders: true, // Return rate limit info in the RateLimit- headers
         legacyHeaders: false, // Disable the 'X-RateLimit' legacy header
-        skip: (req, res) => req.headers["content-length"] > 91
+        skip: (req, res) => req.headers["content-length"] > 200
         ? res.status(413).json({ status: 413, message: "Content too large" })
         : false,
         message: { status: 429, message: 'Too many requests from this IP, please try again after 15 minutes' },
