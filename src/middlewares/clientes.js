5e35590a1a58b110d6ec6b1775ad9c7b16dde7b7
Braian-Quintian <bquintianromero@gmail.com>
2023-08-13 17:26:32 -0500

    feat: :sparkles: Se añade un tiempo de espera para las solicitudes

--- src/middlewares/clientes.js
+++ src/middlewares/clientes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { methodsClientes } from '../routes/clientes.routes.js';
+import {limitGet, limitPost} from '../rules/reglas.js';
 const router = Router();
 
-router.get('/', methodsClientes.getClientes);
-router.get('/alquiler', methodsClientes.getClientesAlquiler);
-router.post('/', methodsClientes.addCliente);
+router.get('/', limitGet(), methodsClientes.getClientes);
+router.get('/alquiler',limitGet(), methodsClientes.getClientesAlquiler);
+router.post('/',limitPost(), methodsClientes.addCliente);
 router.get('/:id', methodsClientes.getClienteById);
 
 export {
